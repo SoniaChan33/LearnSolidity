78240a7d6610fbcd25789aeecd82cb23e04085d2
SoniaChan33 <soniachan33@126.com>
2025-07-04 14:43:45 +0800

    fe:optimizing variables by constant and immutable

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -5,10 +5,11 @@ import {PriceConvertor} from "./PriceConvertor.sol";
 
 contract FundMe{
 
-    address public owner;
+    // immutable 适用于部署时设置且不变的值 
+    address public immutable i_owner;
 
     constructor () {
-        owner = msg.sender;
+        i_owner = msg.sender;
     }
 
     // 表示所有uint256类型可以调用该library的函数
@@ -18,10 +19,11 @@ contract FundMe{
 
     mapping(address funderAddress => uint256 funderAmount) public funderAmountMap;
 
-    uint256 public  minimumUsd = 5 * 1e18;
+    // constant 使用于在编译时已确认的值 并且需要大写
+    uint256 public constant  MINIMUM_USD = 5 * 1e18;
     
     function fund() public payable {
-        require(msg.value.getConversionRate() > minimumUsd, "Didn't send enough ETH"); //if the condition is false, revert with the error message    }
+        require(msg.value.getConversionRate() > MINIMUM_USD, "Didn't send enough ETH"); //if the condition is false, revert with the error message    }
     
         funders.push(msg.sender);
         funderAmountMap[msg.sender] += msg.value;
@@ -49,7 +51,7 @@ contract FundMe{
 
     // 任何由onlyOwner修饰都会先执行这部分代码，“_;”代表被修饰的函数的代码，放在require之前就是先执行完函数代码再执行modifier代码块中内容
     modifier onlyOwner {
-        require(msg.sender == owner,"Must be the Owner!");
+        require(msg.sender == i_owner,"Must be the Owner!");
         _;
     }
 }
